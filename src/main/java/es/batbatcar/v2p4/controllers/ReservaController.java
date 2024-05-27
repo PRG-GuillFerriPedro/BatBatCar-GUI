ac09900c831ef7681599eef7415584d42d745c4c
pedroporo <pedroguillferri9@gmail.com>
2024-05-27 20:41:05 +0200

    Funcion para ver reservas completada

--- src/main/java/es/batbatcar/v2p4/controllers/ReservaController.java
+++ src/main/java/es/batbatcar/v2p4/controllers/ReservaController.java
@@ -1,14 +1,35 @@
 package es.batbatcar.v2p4.controllers;
 
-import es.batbatcar.v2p4.modelo.repositories.ViajesRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import es.batbatcar.v2p4.modelo.dto.viaje.Viaje;
+import es.batbatcar.v2p4.modelo.repositories.ViajesRepository;
 
 @Controller
 public class ReservaController {
 
-    @Autowired
-    private ViajesRepository viajesRepository;
+	@Autowired
+	private ViajesRepository viajesRepository;
 
+	@GetMapping("reservas")
+	public String getReservasAction(@RequestParam int codViaje, Model model) {
+		Viaje v = new Viaje(codViaje);
+		model.addAttribute("viaje", v);
+		model.addAttribute("reservas", viajesRepository.findReservasByViaje(v));
+		model.addAttribute("titulo", "Listado de Reservas del viaje " + codViaje);
+		return "reserva/listado";
+	}
+
+	@GetMapping("reserva-add")
+	public String addReservaAction(@RequestParam int codViaje, Model model) {
+		Viaje v = new Viaje(codViaje);
+		model.addAttribute("viaje", v);
+		model.addAttribute("reservas", viajesRepository.findReservasByViaje(v));
+		model.addAttribute("titulo", "Listado de Reservas del viaje " + codViaje);
+		return "reserva/listado";
+	}
 }
